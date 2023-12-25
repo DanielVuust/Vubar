efe1e7967f3b7ae273c88c4bbc5eeb118e95c781
DanielVuust <Daniel.vuust@gmail.com>
2023-12-26 00:49:04 +0100

    Github deploy test 2

--- src/app/models/cocktails/cocktails-section.spec.ts
+++ src/app/models/cocktails/cocktails-section.spec.ts
@@ -2,6 +2,6 @@ import { CocktailsSection } from './cocktails-section';
 
 describe('CocktailsSection', () => {
   it('should create an instance', () => {
-    expect(new CocktailsSection()).toBeTruthy();
+    expect(new CocktailsSection("test", "test", [])).toBeTruthy();
   });
 });
