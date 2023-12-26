503254852e7301d50ce1a6f7b4cba13e56b3157f
DanielVuust <Daniel.vuust@gmail.com>
2023-12-26 17:33:21 +0100

    Azure deploy test

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,9 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
 import { AboutUsComponent } from './about-us/about-us.component';
 import { HomeComponent } from './home/home.component';
 import { OrderComponent } from './order/order.component';
 import { CocktailMenuComponent } from './cocktail-menu/cocktail-menu.component';
+import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   { path: 'home', component: HomeComponent },
